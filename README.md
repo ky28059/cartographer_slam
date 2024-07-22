d5767fb2601b2fc084ca99ae0ae5531c773fdbcc
Kevin Yu <kevinyu567@gmail.com>
2024-07-22 11:12:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cartographer_slam
+ Cartographer SLAM on the MIT RACECAR Neo cars.
---
+++ cartographer_slam/display.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from cartographer_slam.')
+
+
+if __name__ == '__main__':
+    main()
