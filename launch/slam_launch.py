00e950efd432d1f5ede4f1ba4b3e5f76b1039b9a
Kevin Yu <kevinyu567@gmail.com>
2024-07-22 11:25:12 -0400

    Preliminary launch file

---
+++ launch/slam_launch.py
@@ -0,0 +1,32 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription, IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    teleop_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([
+        os.path.join(get_package_share_directory('racecar_neo'), 'launch'),
+        '/teleop.launch.py'
+    ]))
+
+    cartographer_node = Node(
+        package='cartographer_ros',
+        executable='cartographer_node',
+    )
+
+    display_node = Node(
+        package='cartographer_slam',
+        executable='display',
+        emulate_tty=True,
+        output='screen'
+    )
+
+    return LaunchDescription([
+        teleop_launch,
+        cartographer_node,
+        display_node
+    ])
